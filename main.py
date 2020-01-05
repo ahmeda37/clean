0273b7742de9f83c998bf23a27f7f027558f0b4c
Abdurahman Ahmed <abdahmed@live.ca>
2020-01-05 15:06:11 -0700

    finished homepage

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+@app.route("/services", methods=["GET"])
+def sevices():
+    return render_template("services.html")
 
 
 if __name__ == '__main__':
