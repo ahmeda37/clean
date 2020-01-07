272fd2216c26abdce7a095fb680e4b389cbc643f
Abdurahman Ahmed <abdahmed@live.ca>
2020-01-06 18:59:19 -0700

    added google maps

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_googlemaps import GoogleMaps, Map
 
 app = Flask(__name__)
+GoogleMaps(app)
 
 
 @app.route("/", methods=["GET"])
@@ -9,13 +11,21 @@ def index():
 
 
 @app.route("/services", methods=["GET"])
-def sevices():
+def services():
     return render_template("services.html")
 
 
 @app.route("/contact", methods=["GET"])
 def contact():
-    return render_template("contact.html")
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=122.1419,
+        markers=[(37.4419,-122.1419)]
+    )
+
+    return render_template("contact.html", mymap=mymap)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
